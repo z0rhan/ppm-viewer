601480dac5c6f18cfcb804f1cac96e9a39e48e62
Zorhan Salami <uknowme1928@gmail.com>
2025-06-24 23:58:04 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+static const std::string s_extension = ".ppm";
+
+bool hasPPMextension(std::string& fileName);
+
+int main (int argc, char *argv[]) {
+    if (argc != 2)
+    {
+        std::cerr << "Usage " << argv[0] << " file.ppm\n";
+        return EXIT_FAILURE;
+    }
+
+    std::string fileName = argv[1];
+    if (!hasPPMextension(fileName))
+    {
+        std::cerr << "Error: .ppm file not found\n";
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "File loaded sucessfully\n";
+    return 0;
+}
+
+bool hasPPMextension(std::string& fileName)
+{
+    if (fileName.length() > s_extension.length())
+    {
+        return fileName.compare(fileName.length() - s_extension.length(), s_extension.length(), s_extension) == 0;
+    }
+
+    return false;
+}
